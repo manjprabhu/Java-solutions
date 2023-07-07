d4a796c592d1ca42a4834f4c536837e5ff37d395
MPB <mp4learning@gmail.com>
2023-07-07 15:21:31 +0530

    FindTheHighestAltitude && ReverseVowelsOfAString

---
+++ app/src/main/java/com/mnj/java_solutions/FindTheHighestAltitude.java
@@ -0,0 +1,28 @@
+package com.mnj.java_solutions;
+
+public class FindTheHighestAltitude {
+
+    /*
+     * There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+     * The biker starts his trip on point 0 with altitude equal 0.
+     * You are given an integer array gain of length n where gain[i] is the net gain in
+     * altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+     *
+     * Input: gain = [-5,1,5,0,-7]
+     * Output: 1
+     * Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.*/
+
+
+    void largestAltitude() {
+        int[] gain = {-5, 1, 5, 0, -7};
+
+        int max =0;
+        int current =0;
+
+        for(int i=0;i<gain.length;i++) {
+            current = current+ gain[i];
+            max = Math.max(current,max);
+        }
+        System.out.println("==>>> Maximum altitude:"+max);
+    }
+}
