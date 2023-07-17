6ea3de7cb7e07e182059c2570681c4ed100369e0
MPB <mp4learning@gmail.com>
2023-07-17 09:30:14 +0530

    FirstNonRepeatingCharInStream

--- app/src/main/java/com/mnj/java_solutions/queue/FirstNonRepeatingCharInStream.java
+++ app/src/main/java/com/mnj/java_solutions/queue/FirstNonRepeatingCharInStream.java
@@ -4,7 +4,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class FirstNonRepeatingCharInStream {
-
+/*
+* Given an input stream A of n characters consisting only of lower case alphabets.
+* While reading characters from the stream,
+*  you have to tell which character has appeared only once in the stream upto that point.
+*  If there is no such character then append '#' to the answer.
+*
+* Input: A = "aabc"
+Output: "a#bb"
+Explanation: For every character first non
+repeating character is as follow-
+"a" - first non-repeating character is 'a'
+"aa" - no non-repeating character so '#'
+"aab" - first non-repeating character is 'b'
+"aabc" - first non-repeating character is 'b'
+* */
     public void FirstNonRepeating() {
         String str = "xx";
 
