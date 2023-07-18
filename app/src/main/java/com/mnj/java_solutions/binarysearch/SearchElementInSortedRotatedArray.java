2181b2ca6c81f4d3605bb95f957a6179432250fc
MPB <mp4learning@gmail.com>
2023-07-18 14:57:01 +0530

    Search in sorted and rotated array

---
+++ app/src/main/java/com/mnj/java_solutions/binarysearch/SearchElementInSortedRotatedArray.java
@@ -0,0 +1,68 @@
+package com.mnj.java_solutions.binarysearch;
+
+public class SearchElementInSortedRotatedArray {
+
+    public void searchElementInSortedRotatedArray1() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 8;
+        int index = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println("==>> Element is present at: " + index);
+    }
+
+
+    /*
+    * First, we identify the sorted half of the array.
+    Once found, we determine if the target is located within this sorted half.
+    If not, we eliminate that half from further consideration.
+    Conversely, if the target does exist in the sorted half, we eliminate the other half.
+    * */
+
+    public void searchElementInSortedRotatedArray2() {
+
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 1;
+
+        int low = 0;
+        int high = nums.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (target == nums[mid]) {
+                result = mid;
+                break;
+            }
+            // Here either left half of the array is sorted or right half is sorted.
+
+            //For left half to be sorted , first element in left half shall be smaller then equal to mid element.
+            if (nums[low] <= nums[mid]) {
+
+                //If below condition is true to then target element lies on left half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+                // Now right half is sorted.
+            } else {
+                //If below condition is true to then target element lies on right half
+                if (nums[mid] <= target && target < nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        System.out.println("==>> Element is present at:: " + result);
+
+    }
+}
---
+++ app/src/main/java/com/mnj/java_solutions/binarysearch/SearchElementInSortedRotatedArray2.java
@@ -0,0 +1,69 @@
+package com.mnj.java_solutions.binarysearch;
+
+/**
+ * Given an integer array arr of size N, sorted in ascending order (may contain duplicate values) and
+ * a target value k. Now the array is rotated at some pivot point unknown to you.
+ * Return True if k is present and otherwise, return False.
+ **/
+public class SearchElementInSortedRotatedArray2 {
+
+    public void searchElementInSortedRotatedArray() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 7;
+
+        int low = 0;
+        int high = nums.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low / 2);
+
+            if (nums[mid] == target) {
+                result = mid;
+                break;
+            }
+
+            /*
+            *
+            * How to handle the edge case arr[low] = arr[mid] = arr[high]:
+
+            In the algorithm, we first check if arr[mid] is the target before identifying the sorted half.
+            *  If arr[mid] is not our target, we encounter this edge case.
+            *  In this scenario, since arr[mid] = arr[low] = arr[high],
+            * it means that neither arr[low] nor arr[high] can be the target.
+            *  To handle this edge case, we simply remove arr[low] and arr[high] from our search space,
+            * without affecting the original algorithm.
+
+            To eliminate elements arr[low] and arr[high],
+            * we can achieve this by simply incrementing the low pointer and decrementing the high pointer by one step.
+            *  We will continue this process until the condition arr[low] = arr[mid] = arr[high] is no longer satisfied.
+            * */
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+
+                //If below condition is true to then target element lies on left half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+                // Now right half is sorted.
+            } else {
+                //If below condition is true to then target element lies on right half
+                if (nums[mid] <= target && target < nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        System.out.println("==>> Element is found at Duplicate:"+result);
+    }
+}
