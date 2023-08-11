b33eeb872425818cd52878ea1f964825169f1863
MPB <mp4learning@gmail.com>
2023-08-11 16:07:11 +0530

    Jump game LC45

---
+++ app/src/main/java/com/mnj/java_solutions/greedy/JumpGameLC45.java
@@ -0,0 +1,26 @@
+package com.mnj.java_solutions.greedy;
+
+/**
+ * You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+ * <p>
+ * Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+ */
+public class JumpGameLC45 {
+
+    public void jump() {
+        int[] nums = {2, 3, 1, 1, 4};
+
+        int current = 0;
+        int farthest = 0;
+        int jumps = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == current) {
+                current = farthest;
+                jumps++;
+            }
+        }
+        System.out.println("==>> Minimum number of jumps to reach the last  :" + jumps);
+    }
+}
--- app/src/main/java/com/mnj/java_solutions/greedy/JumpGameLC55.java
+++ app/src/main/java/com/mnj/java_solutions/greedy/JumpGameLC55.java
@@ -1,7 +1,27 @@
 package com.mnj.java_solutions.greedy;
 
+/**
+ * You are given an integer array nums. You are initially positioned at the array's first index,
+ * and each element in the array represents your maximum jump length at that position.
+ * <p>
+ * Return true if you can reach the last index, or false otherwise.
+ * <p>
+ * Input: nums = [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ * <p>
+ * Input: nums = [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what.
+ * Its maximum jump length is 0, which makes it impossible to reach the last index.
+ */
 public class JumpGameLC55 {
 
+    /**
+     * nums[i] is the maximum number of jumps that you can make to the right, and i is the distance from the left,
+     * also your index is calculated from left end.
+     * So, your i (distance of nums[i[ from 0) + nums[i] (max. no. of jumps you can make to right) >= index (position you want to reach)
+     **/
     public void canJump() {
         int[] nums = {2, 3, 1, 1, 4};
         int lastGoodIndex = nums.length - 1;
