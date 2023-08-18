8ad1c2affb19c314a9be1a98f958ce400d271483
MPB <mp4learning@gmail.com>
2023-08-18 10:04:44 +0530

    Leaders in an Array 18082023

--- app/src/main/java/com/mnj/java_solutions/array/LeadersInAnArray18082023.java
+++ app/src/main/java/com/mnj/java_solutions/array/LeadersInAnArray18082023.java
@@ -1,12 +1,13 @@
 package com.mnj.java_solutions.array;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Given an array, print all the elements which are leaders.
  * A Leader is an element that is greater than all of the elements on its right side in the array.
  **/
-public class LeadersInAnArray {
+public class LeadersInAnArray18082023 {
 
     public void leadersInAnArray() {
         int arr[] = {10, 22, 12, 3, 0, 6};
@@ -24,17 +25,16 @@ public class LeadersInAnArray {
             if (isLeader)
                 result.add(arr[i]);
         }
-
         for (int i = 0; i < result.size(); i++)
             System.out.println("==>> Leaders in array are :" + result.get(i));
     }
 
     public void leadersInAnArray2() {
-        int arr[] = {10, 22, 12, 3, 0, 6};
+        int[] arr = {16, 17, 4, 3, 5, 2};
         ArrayList<Integer> result = new ArrayList<>();
 
         result.add(arr[arr.length - 1]);
-        int greatest = arr.length - 1;
+        int greatest = arr[arr.length - 1];
 
         for (int i = arr.length - 2; i >= 0; i--) {
             if (arr[i] > greatest) {
@@ -42,7 +42,7 @@ public class LeadersInAnArray {
                 greatest = arr[i];
             }
         }
-
+        Collections.reverse(result);
         for (int i = 0; i < result.size(); i++)
             System.out.println("==>> Leaders in Array are :" + result.get(i));
     }
