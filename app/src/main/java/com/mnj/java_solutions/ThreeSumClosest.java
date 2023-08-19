b6c863b19eecb8c36d598ecb4ede7dfb19f02437
MPB <mp4learning@gmail.com>
2023-08-19 10:51:21 +0530

    Three sum closest

--- app/src/main/java/com/mnj/java_solutions/ThreeSumClosest.java
+++ app/src/main/java/com/mnj/java_solutions/ThreeSumClosest.java
@@ -2,8 +2,22 @@ package com.mnj.java_solutions;
 
 import java.util.Arrays;
 
+/**
+ * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ * <p>
+ * Return the sum of the three integers.
+ * <p>
+ * You may assume that each input would have exactly one solution.
+ *
+ * Input: nums = [-1,2,1,-4], target = 1
+ * Output: 2
+ * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *
+ * Input: nums = [0,0,0], target = 1
+ * Output: 0
+ * Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ **/
 public class ThreeSumClosest {
-
 
     void threeSumClosest() {
 
