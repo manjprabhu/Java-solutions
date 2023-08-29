6e7ced258fa4dcca8c143b2b938614e768d02116
MPB <mp4learning@gmail.com>
2023-08-29 17:53:50 +0530

    Pascal triangle optimized solution

--- app/src/main/java/com/mnj/java_solutions/PascalsTriangleLC118.java
+++ app/src/main/java/com/mnj/java_solutions/PascalsTriangleLC118.java
@@ -45,4 +45,53 @@ public class PascalsTriangleLC118 {
         }
         return result;
     }
+
+    //********************************************************************
+
+    public void pascalTriangle() {
+        List<List<Integer>> result = new ArrayList<>();
+        int numRows = 6;
+
+        if (numRows == 0) {
+            display(result);
+            return;
+        }
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        result.add(firstRow);
+
+        if (numRows == 1) {
+            display(result);
+            return;
+        }
+
+        for (int i = 1; i < numRows; i++) {
+
+            List<Integer> prevRow = result.get(i - 1); // get the previous row
+
+            // Start the next row
+            ArrayList<Integer> row = new ArrayList<>();
+            row.add(1); // In pascal triangle first element is always 1
+
+            // this loop is to fill the rest of the element in a roq ( i.e barring first and last element)
+            for (int j = 0; j < i - 1; j++) {
+                //ith element  = ith element of previous row  + (i-1)th element of previous row
+                row.add(prevRow.get(j) + prevRow.get(j + 1));
+            }
+            row.add(1);  // In pascal triangle last  element is always 1
+
+            // Add this new row to final result
+            result.add(row);
+        }
+    }
+
+    private void display(List<List<Integer>> ans) {
+        for (List<Integer> it : ans) {
+            for (int ele : it) {
+                System.out.print("" + ele + " ");
+            }
+            System.out.println();
+        }
+    }
 }
