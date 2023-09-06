084760166aae0dd395a900b4564b486ef879b105
MPB <mp4learning@gmail.com>
2023-09-06 12:46:41 +0530

    Ugly number LC262

---
+++ app/src/main/java/com/mnj/java_solutions/UglyNumberLC263.java
@@ -0,0 +1,50 @@
+package com.mnj.java_solutions;
+
+/**
+ * An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+ * <p>
+ * Given an integer n, return true if n is an ugly number.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 6
+ * Output: true
+ * Explanation: 6 = 2 × 3
+ * Example 2:
+ * <p>
+ * Input: n = 1
+ * Output: true
+ * Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+ * Example 3:
+ * <p>
+ * Input: n = 14
+ * Output: false
+ * Explanation: 14 is not ugly since it includes the prime factor 7.
+ **/
+public class UglyNumberLC263 {
+
+    public void isUgly() {
+        int n = 70;
+
+        if (n < 1) {
+            System.out.println("==>> Not an ugly number ");
+            return;
+        }
+
+        while (n % 2 == 0)
+            n = n / 2;
+        while (n % 3 == 0)
+            n = n / 3;
+        while (n % 5 == 0)
+            n = n / 5;
+
+        if (n != 1) {
+            System.out.println("==>> Not an ugly number ");
+        } else {
+            System.out.println("==>> Ugly number ");
+        }
+
+    }
+}
