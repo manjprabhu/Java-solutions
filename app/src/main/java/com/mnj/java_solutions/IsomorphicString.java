eb1544fec4a428810bbc157c5f402ccc6f1178bb
MPB <mp4learning@gmail.com>
2023-11-11 10:23:21 +0530

    Isomorphic string 11112023

--- app/src/main/java/com/mnj/java_solutions/IsomorphicString.java
+++ app/src/main/java/com/mnj/java_solutions/IsomorphicString.java
@@ -3,6 +3,30 @@ package com.mnj.java_solutions;
 import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * Given two strings 'str1' and 'str2', check if these two strings are isomorphic to each other.
+ * <p>
+ * If the characters in str1 can be changed to get str2, then two strings, str1 and str2, are isomorphic. A character must be completely swapped out for another character while maintaining the order of the characters. A character may map to itself, but no two characters may map to the same character.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * str1 = aab
+ * str2 = xxy
+ * Output:
+ * 1
+ * Explanation:
+ * There are two different characters in aab and xxy, i.e a and b with frequency 2 and 1 respectively.
+ * Example 2:
+ * <p>
+ * Input:
+ * str1 = aab
+ * str2 = xyz
+ * Output:
+ * 0
+ * Explanation:
+ * There are two different characters in aab but there are three different charactersin xyz. So there won't be one to one mapping between str1 and str2.
+ **/
 public class IsomorphicString {
 
     void isIsomorphic() {
