7d6bb5b9d1efb14cf448577e12299c16862e39db
MPB <mp4learning@gmail.com>
2023-12-23 18:11:22 +0530

    CountMoreThan n/k Occurences23122023

--- app/src/main/java/com/mnj/java_solutions/array/CountMoreThanOccurences23122023.kt
+++ app/src/main/java/com/mnj/java_solutions/array/CountMoreThanOccurences23122023.kt
@@ -2,7 +2,7 @@ package com.mnj.java_solutions.array
 
 class CountMoreThanOccurences23122023 {
 
-    fun countOccurence(arr: IntArray, n: Int, k: Int): Int {
+    fun countOccurence(arr: IntArray, n: Int, k: Int) {
         var k = k
         val hm: HashMap<Int, Int> = HashMap()
         k = n / k
