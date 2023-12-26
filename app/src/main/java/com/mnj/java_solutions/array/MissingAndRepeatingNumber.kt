8fe82f397a8f509ddd5e0defadd1a6726079a9a2
MPB <mp4learning@gmail.com>
2023-12-26 09:56:59 +0530

    MissingAndRepeatingNumber

---
+++ app/src/main/java/com/mnj/java_solutions/array/MissingAndRepeatingNumber.kt
@@ -0,0 +1,57 @@
+package com.mnj.java_solutions.array
+
+class MissingAndRepeatingNumber {
+    fun missingAndRepeatedNumber() {
+        val arr = intArrayOf(3, 1, 3)
+        val n = arr.size
+        for (i in 1..n) {
+            var count = 0
+            for (k in arr) {
+                if (k == i) {
+                    count++
+                }
+            }
+            if (count == 2) println("==>> Repeated Number is :$i") else if (count == 0) println("==>> Missing Number is :$i")
+        }
+    }
+
+    fun missingAndRepeatedNumber2() {
+        val arr = intArrayOf(4, 3, 6, 2, 1, 1)
+        val n = 6
+        val count = IntArray(n + 1)
+        for (num in arr) {
+            count[num]++
+        }
+        for (i in 1 until count.size) {
+            if (count[i] == 0) {
+                println("==>>2 Missing number is :$i")
+            }
+            if (count[i] == 2) {
+                println("==>>2 Repeated number is :$i")
+            }
+        }
+    }
+
+    fun missingAndRepeatedNumber3() {
+        val arr = intArrayOf(4, 3, 6, 2, 1, 1)
+        val n = 6
+
+        //s1-sum  = x-y
+        //s2-sum2n
+        var s1: Long = 0
+        var s2: Long = 0
+        val sum2n =
+            (n * (n + 1) * (2 * n + 1) / 6).toLong() // sum of squares of first N natural numbers
+        val sum = (n * (n + 1) / 2).toLong() // sum  of first N natural numbers
+        for (num in arr) {
+            s1 = s1 + num
+            s2 = s2 + num.toLong() * num.toLong()
+        }
+        val val1 = s1 - sum //x-y
+        var val2 = s2 - sum2n
+        val2 = val2 / val1 //x +y;
+        val x = (val1 + val2) / 2
+        val y = x - val1
+        println("==>> Repeated number is :$x missing number is  :$y")
+    }
+}
--- app/src/main/java/com/mnj/java_solutions/array/MostFrequentElementsLC347.java
+++ app/src/main/java/com/mnj/java_solutions/array/MostFrequentElementsLC347.java
@@ -1,48 +1,39 @@
-package com.mnj.java_solutions.array;
+package com.mnj.java_solutions.array
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.ArrayList
+import java.util.HashMap
 
-public class MostFrequentElementsLC347 {
-
-    public void topKFrequent() {
-        int[] arr = {1, 1, 1, 2, 2, 3};
-        int k = 2;
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer>[] bucket = new List[arr.length + 1];
-
-        for (int j : arr) {
-            map.put(j, map.getOrDefault(j, 0) + 1);
+class MostFrequentElementsLC347 {
+    fun topKFrequent() {
+        val arr = intArrayOf(1, 1, 1, 2, 2, 3)
+        val k = 2
+        val map = HashMap<Int, Int>()
+        val bucket: Array<MutableList<Int>?> = arrayOfNulls<MutableList<*>?>(arr.size + 1)
+        for (j in arr) {
+            map[j] = map.getOrDefault(j, 0) + 1
         }
-
-        for (int key : map.keySet()) {
-            int frequency = map.get(key);
+        for (key in map.keys) {
+            val frequency = map[key]!!
 
             // each bucket item itself is an array
             if (bucket[frequency] == null) {
-                bucket[frequency] = new ArrayList<>();
+                bucket[frequency] = ArrayList()
             }
-            bucket[frequency].add(key);
+            bucket[frequency]!!.add(key)
         }
-
-        int[] result = new int[k];
-
-        int counter = 0;
-
-        for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
+        val result = IntArray(k)
+        var counter = 0
+        var pos = bucket.size - 1
+        while (pos >= 0 && counter < k) {
             if (bucket[pos] != null) {
-
-                for (int i : bucket[pos]) {
-                    result[counter++] = i;
+                for (i in bucket[pos]!!) {
+                    result[counter++] = i
                 }
             }
+            pos--
         }
-
-        for (int i : result) {
-            System.out.println("==>> :" + i);
+        for (i in result) {
+            println("==>> :$i")
         }
-
     }
 }
