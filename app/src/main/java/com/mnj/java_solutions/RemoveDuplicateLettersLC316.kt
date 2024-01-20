39d824f8b090c1ffd53ee37e11bba678dbe38dba
MPB <mp4learning@gmail.com>
2024-01-20 13:54:50 +0530

    Update NumberOfGoodPairsLC1512

--- app/src/main/java/com/mnj/java_solutions/RemoveDuplicateLettersLC316.kt
+++ app/src/main/java/com/mnj/java_solutions/RemoveDuplicateLettersLC316.kt
@@ -1,31 +1,26 @@
-package com.mnj.java_solutions;
+package com.mnj.java_solutions
 
-import java.util.Stack;
+import java.util.Stack
 
 /**
  * Given a string s, remove duplicate letters so that every letter appears once and only once.
  * You must make sure your result is
  * the smallest in lexicographical order among all possible results.
- **/
+ */
 //1081. Smallest Subsequence of Distinct Characters
-public class RemoveDuplicateLettersLC316 {
-
-    public void removeDuplicateLetters() {
-        String str = "bcabc";
-
-        int[] lastIndex = new int[26];// stores the last occurrence index for every char
-        boolean[] visited = new boolean[26];
+class RemoveDuplicateLettersLC316 {
+    fun removeDuplicateLetters() {
+        val str = "bcabc"
+        val lastIndex = IntArray(26) // stores the last occurrence index for every char
+        val visited = BooleanArray(26)
 
         //calculate the last occurrence index.
-        for (int i = 0; i < str.length(); i++)
-            lastIndex[str.charAt(i) - 'a'] = i;
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            int c = str.charAt(i) - 'a';
-            if (visited[c]) continue;
-            visited[c] = true;
+        for (i in 0 until str.length) lastIndex[str[i].code - 'a'.code] = i
+        val stack = Stack<Int>()
+        for (i in 0 until str.length) {
+            val c = str[i].code - 'a'.code
+            if (visited[c]) continue
+            visited[c] = true
 
             //check if top element is stack is lexcograpically greater than current char
             // then check if stack top element exists on right side of current char in staring
@@ -33,13 +28,12 @@ public class RemoveDuplicateLettersLC316 {
             //in above string we push b and c to stack , when current char is a , check in stack that if c (stack top element)
             // exists in the given string on right side of current char i.e a,
             while (!stack.isEmpty() && stack.peek() > c && lastIndex[stack.peek()] > i) {
-                visited[stack.pop()] = false;
+                visited[stack.pop()] = false
             }
-            stack.push(c);
+            stack.push(c)
         }
-        StringBuilder result = new StringBuilder();
-        for (int l : stack)
-            result.append((char) (l + 'a'));
-        System.out.println("==>> Result string is  :" + result);
+        val result = StringBuilder()
+        for (l in stack) result.append((l + 'a'.code).toChar())
+        println("==>> Result string is  :$result")
     }
 }
