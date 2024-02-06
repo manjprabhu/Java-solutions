97d17f4aa707783c4c6c5a599a36a90ae41bbce1
MPB <mp4learning@gmail.com>
2024-02-06 11:30:59 +0530

    Updated ConsecutiveCharactersLC1446

---
+++ app/src/main/java/com/mnj/java_solutions/string/ConsecutiveCharactersLC1446.kt
@@ -0,0 +1,38 @@
+package com.mnj.java_solutions.string
+
+/**
+ * The power of the string is the maximum length of a non-empty substring that contains only one
+ * unique character.
+ *
+ * Given a string s, return the power of s.
+ *
+ * Example 1:
+ *
+ * Input: s = "leetcode"
+ * Output: 2
+ * Explanation: The substring "ee" is of length 2 with the character 'e' only.
+ * Example 2:
+ *
+ * Input: s = "abbcccddddeeeeedcba"
+ * Output: 5
+ * Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ *
+ */
+class ConsecutiveCharactersLC1446 {
+    fun maxPower() {
+        val s = "abbcccddddeeeeedcba"
+        var count = 1
+        var max_count = 1
+        var prev = s[0]
+        for (i in 1 until s.length) {
+            if (s[i] == prev) {
+                count++
+                max_count = max_count.coerceAtLeast(count)
+            } else {
+                count = 1
+                prev = s[i]
+            }
+        }
+        println("==>> Max count is :$max_count")
+    }
+}
