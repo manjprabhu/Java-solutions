2b3f515f189480a15a6e1db98ba5c7afbbbc5112
MPB <mp4learning@gmail.com>
2024-11-11 18:11:19 +0530

    Pair of numbers with given sum using hashset

--- app/src/main/java/com/mnj/java_solutions/array/PairWithGivenSum.kt
+++ app/src/main/java/com/mnj/java_solutions/array/PairWithGivenSum.kt
@@ -5,12 +5,30 @@ class PairWithGivenSum {
         val sum = 16
         val map = HashMap<Int, Int>()
         for (i in arr.indices) {
-            if (map.containsKey(sum - arr[i])) {
-                println("==>> Pair with given sum: " + arr[i] + " And " + arr[map[sum - arr[i]]!!])
+            val diff = sum -arr[i]
+            if (map.containsKey(diff)) {
+               // println("==>> Pair with given sum: " + arr[i] + " And " + arr[map[sum - arr[i]]!!])
+                println("==>> Pair with given sum: " + arr[i] + " And " +arr[map[diff]!!])
                 return
             }
             map[arr[i]] = i
         }
         println("==>> Pair with given sum does NOT exists")
+    }
+
+    fun hasArrayTwoCandidatesTwo() {
+        println("==>>hasArrayTwoCandidatesTwo")
+        val arr = intArrayOf(1,4,45,10,6,8,12)
+        val sum  = 16
+        val set = HashSet<Int>()
+
+        for(num in arr) {
+            val diff = sum -num
+            if(set.contains(diff)) {
+                println("==>> Pair with given sum: $num And $diff")
+                return
+            }
+            set.add(num)
+        }
     }
 }
