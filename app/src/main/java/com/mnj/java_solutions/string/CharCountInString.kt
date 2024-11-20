84fe2f3d13de5fa8014ade921fbaebe41b20dd5b
MPB <mp4learning@gmail.com>
2024-11-20 12:37:33 +0530

    Char count in a String

--- app/src/main/java/com/mnj/java_solutions/string/CharCountInString.kt
+++ app/src/main/java/com/mnj/java_solutions/string/CharCountInString.kt
@@ -15,4 +15,18 @@ class CharCountInString {
             println("==>> Element $ele appears $cnt times")
         }
     }
+
+
+    fun charCountSolutionTwo() {
+        val str = "helloleetcode"
+        val map = HashMap<Char, Int>()
+
+        for (ele in str)
+            map[ele] = map.getOrDefault(ele, 0) + 1
+
+        for (ele in map) {
+            val cnt = ele.value
+            println("==>>2  Element ${ele.key} appears $cnt times")
+        }
+    }
 }
